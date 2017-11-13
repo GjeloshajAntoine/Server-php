725fd9ecc846a7a3c5eebfc6c0bb29d0f791a6c1
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-11-13 15:03:40 +0100

    Création formulaire login - formulaire signin - header avec 3 boutons (se connecter, se logger et se deconnecter)

--- views/formulaire_login.php
+++ views/formulaire_login.php
@@ -5,25 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css"> 
+    <!-- <link rel="stylesheet" href="style.css">-->
     <title>Formulaire Login</title>
   </head>
 
   <body>
+
+    <h2>Formulaire de connection</h2>
+
     <form action="../controller/log_in.php" method="post">
 
       <div>
         <label for="pseudo">Pseudo : </label>
-        <input type = "text" id="pseudo" />
+        <input type = "text" id="pseudo" placeholder="Votre pseudo" />
       </div>
 
       <div>
         <label for="pass">Mot de passe :</label>
-        <input type = "pass" id="pass" />
+        <input type = "password" id="pass" placeholder="Votre mot de passe" />
       </div>
 
       <div class="button">
-        <button type="submit">Se connecter</button>
+        <input type="submit" value="Se connecter"/>
       </div>
 
     </form>
--- views/formulaire_signin.php
+++ views/formulaire_signin.php
@@ -5,30 +5,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css"> 
+    <!--<link rel="stylesheet" href="style.css">-->
     <title>Formulaire Signin</title>
   </head>
 
   <body>
+
+    <h2>Formulaire d'inscription</h2>
+
     <form action="../controller/new_user.php" method="post">
 
       <div>
         <label for="pseudo">Pseudo : </label>
-        <input type = "text" id="pseudo" />
+        <input type = "text" id="pseudo" placeholder="Votre pseudo" />
       </div>
 
       <div>
         <label for="email">Email : </label>
-        <input type = "email" id="email" />
+        <input type = "email" id="email" placeholder="Votre email" />
       </div>
 
       <div>
         <label for="pass">Mot de passe :</label>
-        <input type = "pass" id="pass" />
+        <input type = "password" id="pass" placeholder="Votre mot de passe"/>
       </div>
 
       <div class="button">
-        <button type="submit">S'enregistrer</button>
+        <input type="submit" value="S'enregister"/>
       </div>
 
     </form>
