de0a808767ba7afa2a4dc56f09f11947999666d4
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2017-11-14 15:59:02 +0100

    accés variable db

--- modeles/modele_message.php
+++ modeles/modele_message.php
@@ -12,6 +12,14 @@ Ajouter le message dans la base de données.
 Récupere le message pour l'afficher dans la chatbox.
 */
 function add($message, $auteur) {
+	try
+	{ //insérer IP valable à la place de localhost
+		$db = new PDO('mysql:host=localhost;dbname=server-php;charset=utf8', 'root', 'root');
+	}
+	catch(Exception $e)
+	{
+	        die('Erreur : '.$e->getMessage());
+	}
 	$req =$db->prepare("INSERT INTO messages (message, auteur, dateEnvoi) VALUES(:message, :auteur, NOW())");
 	$req->execute([
 	  "message"=>$message,
@@ -19,9 +27,19 @@ function add($message, $auteur) {
 	]);
 }
 function recup() {
+	try
+	{ //insérer IP valable à la place de localhost
+		$db = new PDO('mysql:host=localhost;dbname=server-php;charset=utf8', 'root', 'root');
+	}
+	catch(Exception $e)
+	{
+	        die('Erreur : '.$e->getMessage());
+	}
 	$req =$db->query("SELECT * FROM messages ORDER BY id DESC LIMIT 0,10");
 
 	$data=$req->fetchAll(); // $data = array dont chaque ligne = 1msg
 	return $data;
 }
+
+var_dump(recup());
  ?>
