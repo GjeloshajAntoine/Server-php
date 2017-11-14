cf2acf6a852c17422bb0fe8cedd9b1ffb098172a
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-11-14 16:44:16 +0100

    include dans new-user.php

--- controller/new_user.php
+++ controller/new_user.php
@@ -2,6 +2,10 @@
 class SigninController{
   public function __construct(){}
   public function run(){
+    if (isset($_POST["pseudo"]))
+    {
+       include "../modeles/modele_user.php";
+    }
     require_once Views . 'formulaire_signin.php';
   }
 }
