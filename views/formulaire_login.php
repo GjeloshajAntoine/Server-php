f55b8a8d7f1b62fc9de50096fa04a6495b4fdcc5
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-11-14 14:22:47 +0100

    Vérification des champs formulaire en Frontend (Javascript)

--- views/formulaire_login.php
+++ views/formulaire_login.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <!-- <link rel="stylesheet" href="style.css"> -->
+    <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
+    <link rel="stylesheet" href="style.css">
     <title>Formulaire Login</title>
   </head>
 
@@ -19,22 +19,23 @@
 
       <div>
         <label for="pseudo">Pseudo : </label>
-        <input type = "text" name="pseudo" id="pseudo"  placeholder="Votre pseudo" onblur="verifPseudo(this)" />
+        <input type = "text" name="pseudo" id="pseudo"  placeholder="Votre pseudo" autofocus onblur="verifPseudo(this)" />
       </div>
 
       <div>
         <label for="pass">Mot de passe :</label>
-        <input type = "password" id="pass" placeholder="Votre mot de passe" />
+        <input type = "password" id="pass" name="pass" placeholder="Votre mot de passe" />
       </div>
 
       <div class="button">
-        <input type="submit" value="Se connecter"/>
+        <input class="button"type="submit" value="Se connecter"/>
       </div>
 
     </form>
 
     <!-- lien vers script javascript -->
-    <script src="assets/js/app.js"></script>
+    <!-- <script src="assets/js/app.js"></script> -->
+    <script src="app.js"></script>
 
   </body>
 
--- views/formulaire_signin.php
+++ views/formulaire_signin.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <!-- <link rel="stylesheet" href="style.css"> -->
+    <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
+    <link rel="stylesheet" href="style.css">
     <title>Formulaire Signin</title>
   </head>
 
@@ -14,33 +14,34 @@
 
     <h2>Formulaire d'inscription</h2>
 
-    <form action="../controller/log_in.php" onsubmit="return verifForm(this)">
-        <!-- permet de bloquer l'envoi du formulaire si l'un des champ n'est pas correctement rempli -->
+        <form action="../controller/log_in.php" onsubmit="return verifForm(this)">
+            <!-- permet de bloquer l'envoi du formulaire si l'un des champ n'est pas correctement rempli -->
 
-      <div>
-        <label for="pseudo">Pseudo : </label>
-        <input type = "text" name="pseudo" id="pseudo" placeholder="Votre pseudo" onblur="verifPseudo(this)" />
-        <!-- onBlur, déclenché lorsque le champ en question perd le focus, on utilise le mot-clé this puisque le champ à vérifier est justement celui qui vient de perdre le focus -->
-      </div>
+          <div>
+            <label for="pseudo">Pseudo : </label>
+            <input type = "text" name="pseudo" id="pseudo" placeholder="Votre pseudo" autofocus onblur="verifPseudo(this)" />
+            <!-- onBlur, déclenché lorsque le champ en question perd le focus, on utilise le mot-clé this puisque le champ à vérifier est justement celui qui vient de perdre le focus -->
+          </div>
 
-      <div>
-        <label for="email">Email : </label>
-        <input type = "text" name="email" size="30" id="email" placeholder="Votre email" onblur="verifMail(this)" />
-      </div>
+          <div>
+            <label for="email">Email : </label>
+            <input type = "text" name="email" size="30" id="email" placeholder="Votre email" onblur="verifMail(this)" />
+          </div>
+          <div id="error_mail"></div>
 
-      <div>
-        <label for="pass">Mot de passe :</label>
-        <input type = "password" id="pass" placeholder="Votre mot de passe"/>
-      </div>
+          <div>
+            <label for="pass">Mot de passe :</label>
+            <input type = "password" name="pass" id="pass" placeholder="Votre mot de passe"/>
+          </div>
 
-      <div class="button">
-        <input type="submit" value="S'enregister"/>
-      </div>
-
+          <div class="button">
+            <input type="submit" value="S'enregister"/>
+          </div>
     </form>
 
     <!-- lien vers script javascript -->
-    <script src="assets/js/app.js"></script>
+    <!-- <script src="assets/js/app.js"></script> -->
+    <script src="app.js"></script>
 
   </body>
 
