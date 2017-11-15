e50203de4e720cdf9b38b96043c3f66bbe294ce9
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-11-15 16:38:36 +0100

    Ajout mise en forme message lié à vérification champs en php

--- controller/new_user.php
+++ controller/new_user.php
@@ -12,20 +12,20 @@ class SigninController{
              //On vérifie si longueur pseudo est bien situé entre 3 et 25 caractères
              if ((strlen ($_POST['pseudo']) >= 3 && (strlen ($_POST['pseudo'])<=25)))
              {
-               echo "La longeur de votre pseudo est bien située entre 3 et 25 caractères !";
+               echo "Info : La longeur de votre pseudo est bien située entre 3 et 25 caractères ! <br />";
 
                //On vérifie que le mail correspond bien aux critères de vérification standard
                if (preg_match("#^[a-z0-9.-]+@[a-z0-9.-]{2,}.[a-z]{2,4}$#", $_POST['email']))
                {
-                 echo "L'email encodée correspond aux critères !";
+                 echo "Info : L'email encodée correspond aux critères ! <br />";
                  add_membres($_POST["pseudo"],$_POST["email"],$_POST["pass"]);//on  appele la fonction qui a été créée dans modele_use.php et on lui indique les paramétre recueillis par method post dans formulaire
-                 echo "Votre inscription s'est déroulée avec succès !";
+                 echo "Confirmation : Votre inscription s'est déroulée avec succès ! <br />";
                }
              }
            }
            else
            {
-             echo "Il y a une erreurs dans l'un des champs ! Veuillez vérifier les données que vous avez encodées!";
+             echo "Warning : Il y a une erreurs dans l'un des champs ! Veuillez vérifier les données que vous avez encodées!";
            }
     }
     require_once Views . 'formulaire_signin.php';
