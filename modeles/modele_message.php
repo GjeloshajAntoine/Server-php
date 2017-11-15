d3d2e672cd14cbf14938d582574ba13f59266b4a
Caroline Bieuvelet <caroline.bieuvelet@gmail.com>
2017-11-15 14:59:08 +0100

    update lien MVC messages
    by Weich, Nico, Caro

--- modeles/modele_message.php
+++ modeles/modele_message.php
@@ -1,17 +1,5 @@
 <?php
-try
-{ //insérer IP valable à la place de localhost
-	$db = new PDO('mysql:host=localhost;dbname=server-php;charset=utf8', 'root', 'root');
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
-/*
-Ajouter le message dans la base de données.
-Récupere le message pour l'afficher dans la chatbox.
-*/
-function add($message, $auteur) {
+function linkDB(){
 	try
 	{ //insérer IP valable à la place de localhost
 		$db = new PDO('mysql:host=localhost;dbname=server-php;charset=utf8', 'root', 'root');
@@ -20,6 +8,13 @@ function add($message, $auteur) {
 	{
 	        die('Erreur : '.$e->getMessage());
 	}
+}
+/*
+Ajouter le message dans la base de données.
+Récupere le message pour l'afficher dans la chatbox.
+*/
+function add($message, $auteur) {
+	linkDB();
 	$req =$db->prepare("INSERT INTO messages (message, auteur, dateEnvoi) VALUES(:message, :auteur, NOW())");
 	$req->execute([//array associatif
 	  "message"=>$message,
@@ -27,14 +22,7 @@ function add($message, $auteur) {
 	]);
 }
 function recup() {
-	try//test si erreur
-	{ //insérer IP valable à la place de localhost
-		$db = new PDO('mysql:host=localhost;dbname=server-php;charset=utf8', 'root', 'root');
-	}
-	catch(Exception $e)//récupére l'érreur l'affiche
-	{
-	        die('Erreur : '.$e->getMessage());
-	}
+	linkDB();
 	$req =$db->query("SELECT * FROM messages ORDER BY id DESC LIMIT 0,10");
 
 	$data=$req->fetchAll(); // $data = array dont chaque ligne = 1msg
