8b98af4a33193abe582d8257b13a2cd04c70d93d
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-11-15 17:55:06 +0100

    Vérification si pseudo et pass pas vide

--- controller/log_in.php
+++ controller/log_in.php
@@ -3,7 +3,11 @@ class LoginController{
   public function __construct(){}
   public function run(){
     require_once Views . 'formulaire_login.php';
-    include "modeles/modele_user.php"
+    include "modeles/modele_user.php";
+    if ($_POST["pseudo"]=="" && $_POST["pseudo"]=="" )
+    {
+      echo "Votre pseudo ou votre mot de passe n'a pas été encodé !";
+    }
     log_in($_POST["pseudo"],$_POST["pass"]);
   }
 }
