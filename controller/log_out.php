4cab9ef64630e1307d378d32c75ea9e1b111704d
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-11-15 18:15:15 +0100

    Logout correction message erreurs

--- controller/log_out.php
+++ controller/log_out.php
@@ -2,7 +2,7 @@
 class LogoutController{
   public function __construct(){}
   public function run(){
-    require_once Views . 'log_in.php';
+    // require_once Views . 'log_in.php';
     session_unset();//détruit toutes les variables de la session courante et donc déconnect l'utilisateur dont le pseudo et mot de passe était stocké dans session.
   }
 }
