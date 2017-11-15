ba6c2e7153d2ca80a57e030b33ff4959652d22a3
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-11-15 18:19:08 +0100

    Ajout message vous n'êtes plus connecté

--- controller/log_out.php
+++ controller/log_out.php
@@ -4,6 +4,7 @@ class LogoutController{
   public function run(){
     // require_once Views . 'log_in.php';
     session_unset();//détruit toutes les variables de la session courante et donc déconnect l'utilisateur dont le pseudo et mot de passe était stocké dans session.
+    echo "Vous n'êtes plus connecté !";
   }
 }
  ?>
