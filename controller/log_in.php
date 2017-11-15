2c18786f925952ac8e8783a2c17ff7881686832b
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-11-15 17:47:45 +0100

    Création fonction log-in

--- controller/log_in.php
+++ controller/log_in.php
@@ -3,6 +3,8 @@ class LoginController{
   public function __construct(){}
   public function run(){
     require_once Views . 'formulaire_login.php';
+    include "modeles/modele_user.php"
+    log_in($_POST["pseudo"],$_POST["pass"]);
   }
 }
  ?>
--- modeles/modele_user.php
+++ modeles/modele_user.php
@@ -13,4 +13,25 @@ function add_membres ($pseudo, $email, $pass)
     "pass"=>$pass
 	]);
 }
+function log_in($pseudo,$pass)
+{
+  $db = new PDO('mysql:host=localhost;dbname=server-php;charset=utf8', 'root', 'root');
+  $req =$db->prepare("SELECT pseudo FROM membres WHERE pseudo= :pseudo AND pass= :pass");
+  $req->execute
+  ([
+    "pseudo"=>$pseudo,
+    "pass"=>$pass
+  ]);
+
+  $data=$req->fetchAll();// Array donc chaque ligne va représenter un utilisateur
+  if count($data)!= 0 // Si il y a une ligne (pas égale à 0) qui correspond, alors l'utilisateur existe et pseudo et pass corresponde à ceux stocké dans la DB
+  {
+    echo "On donne accès en écriture au chatbox";
+  }
+  else
+  {
+    echo "On ne donne pas accès en écriture au chatbox";
+  }
+//  $number_of_rows = count($ps->fetchAll());
+}
 ?>
